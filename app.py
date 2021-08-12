c096b7608a46071f0dad365f4b0e654ca886576c
Michael <mkshahid@usc.edu>
2021-08-12 00:34:00 -0700

    added export key

--- app.py
+++ app.py
@@ -19,9 +19,9 @@ db = firebase.database()
 
 app = Flask(__name__)
 
-SPOTIPY_CLIENT_ID='ac699f4f7bf7436380637c9e0720bc67'
-SPOTIPY_CLIENT_SECRET='9c14670bdf2042c6a4b0e7463d4eadd2'
-SPOTIPY_REDIRECT_URI='http://localhost:8888/callback'
+export SPOTIPY_CLIENT_ID='ac699f4f7bf7436380637c9e0720bc67'
+export SPOTIPY_CLIENT_SECRET='9c14670bdf2042c6a4b0e7463d4eadd2'
+export SPOTIPY_REDIRECT_URI='http://localhost:8888/callback'
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
