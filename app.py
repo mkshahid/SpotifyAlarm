4b00caf441c2d8ada411d9633ca6b52584e9dd12
mkshahid <77810731+mkshahid@users.noreply.github.com>
2021-08-23 22:15:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,6 @@ def test():
 
 @app.route('/', methods=['GET', 'POST'])
 def basic():
-
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
     id = sp.current_user()['id']
     todo = db.child(id).get()
@@ -52,28 +51,13 @@ def basic():
                 to.append(key)
             else:
                 tokensAdded = True
-   if not tokensAdded:
-       if not request.args.get('access_token'):
-           import math
-           from random import random
-
-           state = ''
-           possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-
-           for i in range(16):
-               state += possible[math.floor(random() * len(possible))]
-
-           return redirect('https://accounts.spotify.com/authorize?' +
-                 'response_type=' + 'code' + '&' +
-                 'client_id=' + cid + '&' +
-                 'scope=' + scope + '&' +
-                 'redirect_uri=' + redirect_uri + '&' +
-                 'state=' + state
-               )
-       else:
-           access_token = request.args.get('access_token')
-           refresh_token = request.args.get('refresh_token')
-           db.child(id).push({"access_token": access_token, "refresh_token": refresh_token})
+    if not tokensAdded:
+        if not request.args.get('access_token'):
+            return redirect("https://spotify-alarm-login.herokuapp.com/", code = 302)
+        else:
+            access_token = request.args.get('access_token')
+            refresh_token = request.args.get('refresh_token')
+            db.child(id).push({"access_token": access_token, "refresh_token": refresh_token})
     devices = sp.devices()
     deviceNames = []
     deviceIds = []
@@ -128,7 +112,7 @@ def basic():
                 for key in keys:
                     if ":" in key:
                         to.append(key)
-            return render_template('userForm.html', t=to, d=duplicate, names=deviceNames, ids=deviceIds, types=deviceTypes, pName=userPlaylistsNames, pId=userPlaylistsIds)
+            return render_template('public/userForm.html', t=to, d=duplicate, names=deviceNames, ids=deviceIds, types=deviceTypes, pName=userPlaylistsNames, pId=userPlaylistsIds)
         elif ':' in request.form['submit']:
             for val in to:
                 if val == request.form['submit']:
@@ -148,11 +132,103 @@ def basic():
 #                    db.child(id).remove(key)
 #            return render_template('public/userForm.html', names=deviceNames, ids=deviceIds, types=deviceTypes)
         if object is None:
-            return render_template('userForm.html', names=deviceNames, ids=deviceIds, types=deviceTypes, pName=userPlaylistsNames, pId=userPlaylistsIds)
-        return render_template('userForm.html', t=to, names=deviceNames, ids=deviceIds, types=deviceTypes, pName=userPlaylistsNames, pId=userPlaylistsIds)
+            return render_template('public/userForm.html', names=deviceNames, ids=deviceIds, types=deviceTypes, pName=userPlaylistsNames, pId=userPlaylistsIds)
+        return render_template('public/userForm.html', t=to, names=deviceNames, ids=deviceIds, types=deviceTypes, pName=userPlaylistsNames, pId=userPlaylistsIds)
     if object is None:
-        return render_template('userForm.html', names=deviceNames, ids=deviceIds, types=deviceTypes, pName=userPlaylistsNames, pId=userPlaylistsIds)
-    return render_template('userForm.html', t=to, names=deviceNames, ids=deviceIds, types=deviceTypes, pName=userPlaylistsNames, pId=userPlaylistsIds)
+        return render_template('public/userForm.html', names=deviceNames, ids=deviceIds, types=deviceTypes, pName=userPlaylistsNames, pId=userPlaylistsIds)
+    return render_template('public/userForm.html', t=to, names=deviceNames, ids=deviceIds, types=deviceTypes, pName=userPlaylistsNames, pId=userPlaylistsIds)
+#     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+#     id = sp.current_user()['id']
+#     todo = db.child(id).get()
+#     object = todo.val()
+#     tokensAdded = False
+#     to = []
+#     if object is not None:
+#         keys = todo.val().keys()
+#         for key in keys:
+#             if ":" in key:
+#                 to.append(key)
+#             else:
+#                 tokensAdded = True
+                
+#     deviceNames = []
+#     deviceIds = []
+#     deviceTypes = []
+#     for d in devices['devices']:
+#         deviceNames.append(d['name'])
+#         deviceIds.append(d['id'])
+#         deviceTypes.append(d['type'])
+#     userPlaylists = {}
+#     userPlaylistsNames = []
+#     userPlaylistsIds = []
+#     currentOffset = 0
+#     while True:
+#         userPlaylists = sp.current_user_playlists(limit=50, offset=currentOffset)
+#         for p in userPlaylists['items']:
+#             userPlaylistsNames.append(p['name'])
+#             userPlaylistsIds.append(p['id'])
+#         if len(userPlaylists['items']) < 50:
+#             break
+#         else:
+#             currentOffset += 50
+#     if request.method == 'POST':
+#         if request.form['submit'] == 'add':
+# #           replace "user" with the user's spotify username that is acquired with api call
+#             time = request.form['time']
+#             playlistType = request.form['playlistType']
+#             period = request.form['timePeriod']
+#             volume = request.form['volume']
+#             device = request.form['devices']
+#             userPlaylistSelection = request.form['playlists']
+#             duplicate = False
+#             if to is not None:
+#                 for alarmTime in to:
+#                     if alarmTime == time:
+#                         duplicate = True
+#             if duplicate != True:
+#                 if playlistType == 'recent_favorites':
+#                     db.child(id).child(time).push(period)
+#                     db.child(id).child(time).push(volume)
+#                     db.child(id).child(time).push(device)
+#                 else:
+#                     db.child(id).child(time).push(userPlaylistSelection)
+#                     db.child(id).child(time).push(volume)
+#                     db.child(id).child(time).push(device)
+#             todo = db.child(id).get()
+#             object = todo.val()
+#             keys = todo.val().keys()
+#             to = []
+#             if object is not None:
+#                 keys = todo.val().keys()
+#                 to = []
+#                 for key in keys:
+#                     if ":" in key:
+#                         to.append(key)
+#             return render_template('userForm.html', t=to, d=duplicate, names=deviceNames, ids=deviceIds, types=deviceTypes, pName=userPlaylistsNames, pId=userPlaylistsIds)
+#         elif ':' in request.form['submit']:
+#             for val in to:
+#                 if val == request.form['submit']:
+#                     db.child(id).child(val).remove()
+#             todo = db.child(id).get()
+#             object = todo.val()
+#             if object is not None:
+#                 keys = todo.val().keys()
+#                 to = []
+#                 for key in keys:
+#                     if ":" in key:
+#                         to.append(key)
+# #        elif request.form['submit'] == 'delete_all':
+# #            keys = todo.val().keys()
+# #            if object is not None:
+# #                for key in to:
+# #                    db.child(id).remove(key)
+# #            return render_template('public/userForm.html', names=deviceNames, ids=deviceIds, types=deviceTypes)
+#         if object is None:
+#             return render_template('userForm.html', names=deviceNames, ids=deviceIds, types=deviceTypes, pName=userPlaylistsNames, pId=userPlaylistsIds)
+#         return render_template('userForm.html', t=to, names=deviceNames, ids=deviceIds, types=deviceTypes, pName=userPlaylistsNames, pId=userPlaylistsIds)
+#     if object is None:
+#         return render_template('userForm.html', names=deviceNames, ids=deviceIds, types=deviceTypes, pName=userPlaylistsNames, pId=userPlaylistsIds)
+#     return render_template('userForm.html', t=to, names=deviceNames, ids=deviceIds, types=deviceTypes, pName=userPlaylistsNames, pId=userPlaylistsIds)
 
 @app.route('/time')
 def get_current_time():
