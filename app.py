92053e7041ad6ca599a4cf93a4d01941e3573d28
mkshahid <77810731+mkshahid@users.noreply.github.com>
2021-08-27 02:33:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,8 @@ def basic():
         access_token = request.args.get('access_token')
         refresh_token = request.args.get('refresh_token')
         id = request.args.get('id')
-        db.child(id).child({"access_token": access_token, "refresh_token": refresh_token}).push("this means nothing")
+        db.child(id).child("access_token": access_token).push("this means nothing")
+        db.child(id).child("refresh_token": refresh_token).push("this means nothing")
     
     
     # fix based on whether token is expired
